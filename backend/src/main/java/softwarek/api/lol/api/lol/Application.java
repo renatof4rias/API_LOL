f36066b880a73c2ed54ff2a1234ebe70f82ca85a
renatof4rias <renatof4rias@gmail.com>
2023-07-26 13:16:46 -0300

    setup iniciado

---
+++ backend/src/main/java/softwarek/api/lol/api/lol/Application.java
@@ -0,0 +1,13 @@
+package softwarek.api.lol.api.lol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
